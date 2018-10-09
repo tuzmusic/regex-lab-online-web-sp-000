8a84027b32fc99c5eb2dd90445e67afea2ad11ce
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-09 17:53:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/^[A-Z]\.+\W\z/).size > 0
+  text.scan(/^[A-Z].+\W\z/).size > 0
 end
 
 def valid_phone_number?(phone)
